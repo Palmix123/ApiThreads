35b382a4765127b75b17931f58652097d67ff9d7
Palmix12 <palmowski.bartosz@wp.pl>
2024-04-24 14:31:03 +0200

    PierwszyCommit

---
+++ WebApplication1/WebApplication1/Controllers/WarehouseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+public class WarehouseController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+    public WarehouseController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+}
---
+++ WebApplication1/WebApplication1/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly IConfiguration _configuration;
+    public ProductRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task<bool> DoesProductExist(int id)
+    {
+        var query = "SELECT 1 FROM Product WHERE ID = @ID";
+        
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        using SqlCommand command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = query;
+        command.Parameters.AddWithValue("@ID", id);
+
+        await connection.OpenAsync();
+
+        var res = await command.ExecuteScalarAsync();
+        
+        return res is not null;
+    }
+}
